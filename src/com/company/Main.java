08e86ff66b37e33f93696b30a74b1e2e495e328f
mechaniktgmobilne <marzkup@gmail.com>
2026-09-08 10:23:51 +0200

    wylosowanie tablicy 10 elementowej

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
+        //wylosuj 10 liczb z zakresuod 1 do 100
+        //tablica
+        int[] tablicaLiczbLosowych = new int[10]; // nie można później zmienić rozmiaru tablicy
+        Random random = new Random();
+        for (int i = 0; i < tablicaLiczbLosowych.length; i++) {
+            tablicaLiczbLosowych[i] = random.nextInt(100)+1;
+        }
+        //wypisz wylosowane liczby na ekranie
+        System.out.println("Tablica liczb losowych");
+        for (int elementTablicy : tablicaLiczbLosowych) {
+            System.out.println(elementTablicy);
+        }
 
     }
 }
