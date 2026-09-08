e53444bbfdb9a59f2ef596e54ab9d04f664f3444
mechaniktgmobilne <marzkup@gmail.com>
2026-09-08 11:20:59 +0200

    wylosowanie zbioru 10 elementowego

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,27 +1,26 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
         //wylosuj 10 liczb z zakresu od 1 do 15 bez powtórzeń
-        //lista
-        ArrayList<Integer> listaLiczbLosowych = new ArrayList<>();
+        //zbiór
+        HashSet<Integer> zbiorLiczbLosowych = new HashSet<>();
+        //zbiór elementy bez powtózeń nieindeksowane
         Random random = new Random();
         int liczba;
         int i = 0;
-        while (listaLiczbLosowych.size()<10) {
+        while (zbiorLiczbLosowych.size()<10) {
             liczba = random.nextInt(15)+1;
-            if(listaLiczbLosowych.contains(liczba)){
-                continue;
-            }
-            listaLiczbLosowych.add(liczba);
+            zbiorLiczbLosowych.add(liczba);
         }
         //wypisz wylosowane liczby na ekranie
-        System.out.println("Lista liczb losowych");
-        System.out.println(listaLiczbLosowych);
+        System.out.println("Zbior liczb losowych");
+        System.out.println(zbiorLiczbLosowych);
 
     }
 }
