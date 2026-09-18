54cc795531e047421e171a11e8458cdf29bd226a
mechaniktgmobilne <marzkup@gmail.com>
2026-09-18 07:49:24 +0200

    wysowanie liczb w klasie

--- src/com/company/Gra.java
+++ src/com/company/Gra.java
@@ -2,6 +2,7 @@ package com.company;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Random;
 
 public class Gra {
     private static HashSet<Integer> wylosowane;
@@ -12,11 +13,29 @@ public class Gra {
         wylosujLiczby();
     }
 
+    public static HashSet<Integer> getWylosowane() {
+        return wylosowane;
+    }
+
+    public ArrayList<Integer> getWpisane() {
+        return wpisane;
+    }
+
+    public ArrayList<Integer> getTrafione() {
+        return trafione;
+    }
+
     /**
      * losuje 6 liczb z zakresu 1 49 jeżeli jeszcze nie wylosowano
      */
     public void wylosujLiczby(){
-
+        if(wylosowane == null) {
+            wylosowane = new HashSet<>();
+            Random random = new Random();
+            while (wylosowane.size() < 6) {
+                wylosowane.add(random.nextInt(49) + 1);
+            }
+        }
     }
 
     /**
@@ -36,10 +55,16 @@ public class Gra {
     //przeciążanie metod -> polimorfizm
     //metody mają taką samą nazwę ale różna liczba argumentów lub ich typ
     public void wypisz(ArrayList<Integer> listaDoWypisania){
-
+        System.out.println(listaDoWypisania);
+        for (int i = 0; i < listaDoWypisania.size(); i++) {
+            System.out.print(listaDoWypisania.get(i)+", ");
+        }
     }
     public void wypisz(HashSet<Integer> zbiorDoWypisania){
-
+        System.out.println(zbiorDoWypisania);
+        for (Integer element :zbiorDoWypisania ) {
+            System.out.print(element+", ");
+        }
     }
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,20 +7,7 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-        //wylosuj 10 liczb z zakresu od 1 do 15 bez powtórzeń
-        //zbiór
-        HashSet<Integer> zbiorLiczbLosowych = new HashSet<>();
-        //zbiór elementy bez powtózeń nieindeksowane
-        Random random = new Random();
-        int liczba;
-        int i = 0;
-        while (zbiorLiczbLosowych.size()<10) {
-            liczba = random.nextInt(15)+1;
-            zbiorLiczbLosowych.add(liczba);
-        }
-        //wypisz wylosowane liczby na ekranie
-        System.out.println("Zbior liczb losowych");
-        System.out.println(zbiorLiczbLosowych);
-
+        Gra gra = new Gra(); //wywołanie konstruktora
+        gra.wypisz(Gra.getWylosowane());
     }
 }
